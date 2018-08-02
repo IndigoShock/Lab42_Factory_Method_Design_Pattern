b1dfd99dc993ff67ac023a9c3f5f7d53e0203dc7
stephendeanharper <stephendeanharper@gmail.com>
2018-08-01 20:30:37 -0700

    Add project files.

---
+++ Lab42_Factory_Method_Design_Pattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab42_Factory_Method_Design_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
